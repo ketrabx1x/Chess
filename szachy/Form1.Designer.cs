b88b5a7940947ff49fe1a9239feec6d7cf06e83e
ketrabx1x <ketrabx1x@gmail.com>
2023-05-26 16:58:26 +0200

    Dodaj pliki projektów.

---
+++ szachy/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace szachy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(412, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "RESET";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(419, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RUCH:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(419, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(514, 401);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Szachy";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ szachy/Form1.cs
@@ -0,0 +1,455 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace szachy
+{
+    public partial class Form1 : Form
+    {
+        public Image chessSprites;
+        public int[,] map = new int[8, 8]
+        {
+            {15,14,13,12,11,13,14,15},
+            {16,16,16,16,16,16,16,16},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {26,26,26,26,26,26,26,26},
+            {25,24,23,22,21,23,24,25},
+        };
+        public Button[,] butts = new Button[8, 8];
+
+        public int player;
+
+        public Button prevButton;
+
+        public bool moving = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Icon = Properties.Resources.szach;
+            chessSprites = new Bitmap(Properties.Resources.chess);
+            
+            Init();
+        }
+
+        public void Init()
+        {
+            map = new int[8, 8]
+            {
+                {15,14,13,12,11,13,14,15},
+                {16,16,16,16,16,16,16,16},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {26,26,26,26,26,26,26,26},
+                {25,24,23,22,21,23,24,25},
+            };
+            player = 1;
+            CreateMap();
+            label2.Text = "BIAŁE";
+        }
+
+        public void CreateMap()
+        {
+            for(int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    butts[i, j] = new Button();
+
+                    Button button = new Button();
+                    button.Size = new Size(50,50);
+                    button.Location = new Point(j*50,i*50);
+
+                    switch (map[i, j]/10)
+                    {
+                        case 1:
+                            Image part = new Bitmap(50, 50);
+                            Graphics g = Graphics.FromImage(part);
+                            g.DrawImage(chessSprites, new Rectangle(0, 0, 50, 50), 0+150*(map[i,j]%10-1),
+                                0, 150, 150, GraphicsUnit.Pixel);
+                            button.BackgroundImage = part;
+                            break;
+                        case 2:
+                            Image part2 = new Bitmap(50, 50);
+                            Graphics g2 = Graphics.FromImage(part2);
+                            g2.DrawImage(chessSprites, new Rectangle(0, 0, 50, 50), 0 + 150 * (map[i, j] % 10 - 1),
+                                150, 150, 150, GraphicsUnit.Pixel);
+                            button.BackgroundImage = part2;
+                            break;
+                    }
+                    button.BackColor = GetPrevButtonColor(button);
+                    button.Click += new EventHandler(OnFigurePress);
+                    this.Controls.Add(button);
+
+                    butts[i,j] = button;
+                }
+            }
+        }
+
+        public Color GetPrevButtonColor(Button prevButton)
+        {
+            if ((prevButton.Location.Y/50 % 2) != 0)
+            {
+                if ((prevButton.Location.X / 50 % 2) == 0)
+                {
+                    return Color.Gray;
+                }
+            }
+            if ((prevButton.Location.Y / 50) % 2 == 0)
+            {
+                if ((prevButton.Location.X / 50) % 2 != 0)
+                {
+                    return Color.Gray;
+                }
+            }
+            return Color.White;
+        }
+
+        public void OnFigurePress(object sender, EventArgs e)
+        {
+            if(prevButton != null)
+                prevButton.BackColor = GetPrevButtonColor(prevButton);
+
+            Button pressed = sender as Button;
+
+            if (map[pressed.Location.Y / 50, pressed.Location.X / 50] != 0 &&
+                map[pressed.Location.Y / 50, pressed.Location.X / 50] / 10 == player)
+            {
+                CloseSteps();
+                pressed.BackColor = Color.Red;
+                DeactivateAllButtons();
+                pressed.Enabled = true;
+                ShowSteps(pressed.Location.Y / 50, pressed.Location.X / 50,
+                    map[pressed.Location.Y / 50, pressed.Location.X / 50]);
+
+                if (moving)
+                {
+                    CloseSteps();
+                    pressed.BackColor = GetPrevButtonColor(pressed);
+                    ActivateAllButtons();
+                    moving = false;
+                }
+                else
+                    moving = true;
+            }
+            else
+            {
+                if (moving)
+                {
+                    int temp = map[pressed.Location.Y/50,pressed.Location.X/50];
+                    map[pressed.Location.Y / 50, pressed.Location.X / 50] = map[prevButton.Location.Y / 50,
+                        prevButton.Location.X / 50];
+
+                    map[prevButton.Location.Y /50, prevButton.Location.X / 50] = temp;
+                    pressed.BackgroundImage = prevButton.BackgroundImage;
+                    prevButton.BackgroundImage = null;
+                    moving = false;
+                    CloseSteps();
+                    ActivateAllButtons();
+                    switchPlayer();
+                }
+            }
+            
+            prevButton = pressed;
+        }
+
+        public void ShowSteps(int IcurrFigure, int JcurrFigure, int currFigure)
+        {
+            int dir = player == 1 ? 1 : -1;
+            switch (currFigure % 10)
+            {
+                case 6:
+                    if (InsideBorder(IcurrFigure + 1 * dir, JcurrFigure))
+                    {
+                        if (map[IcurrFigure + 1 * dir, JcurrFigure] == 0)
+                        {
+                            butts[IcurrFigure + 1 * dir, JcurrFigure].BackColor = Color.Yellow;
+                            butts[IcurrFigure + 1 * dir, JcurrFigure].Enabled = true;
+                        }
+                    }
+
+                    if (InsideBorder(IcurrFigure + 1 * dir, JcurrFigure + 1))
+                    {
+                        if (map[IcurrFigure + 1 * dir, JcurrFigure + 1] != 0 &&
+                            map[IcurrFigure + 1 * dir, JcurrFigure + 1] / 10 != player)
+                        {
+                            butts[IcurrFigure + 1 * dir, JcurrFigure + 1].BackColor = Color.Yellow;
+                            butts[IcurrFigure + 1 * dir, JcurrFigure + 1].Enabled = true;
+                        }
+                    }
+                    if (InsideBorder(IcurrFigure + 1 * dir, JcurrFigure - 1))
+                    {
+                        if (map[IcurrFigure + 1 * dir, JcurrFigure - 1] != 0 &&
+                            map[IcurrFigure + 1 * dir, JcurrFigure - 1] / 10 != player)
+                        {
+                            butts[IcurrFigure + 1 * dir, JcurrFigure - 1].BackColor = Color.Yellow;
+                            butts[IcurrFigure + 1 * dir, JcurrFigure - 1].Enabled = true;
+                        }
+                    }
+                    break;
+                case 5:
+                    ShowVerticalHorizontal(IcurrFigure, JcurrFigure);
+                    break;
+
+                case 3:
+                    ShowDiagonal(IcurrFigure, JcurrFigure);
+                    break;
+
+                case 2:
+                    ShowVerticalHorizontal(IcurrFigure, JcurrFigure);
+                    ShowDiagonal(IcurrFigure, JcurrFigure);
+                    break;
+
+                case 1:
+                    ShowVerticalHorizontal(IcurrFigure, JcurrFigure, true);
+                    ShowDiagonal(IcurrFigure, JcurrFigure, true);
+                    break;
+
+                case 4:
+                    ShowHorseSteps(IcurrFigure, JcurrFigure);
+                    break;
+            }
+        }
+
+        public void ShowHorseSteps(int IcurrFigure, int JcurrFigure)
+        {
+            if (InsideBorder(IcurrFigure - 2, JcurrFigure + 1))
+            {
+                DeterminePath(IcurrFigure - 2, JcurrFigure + 1);
+            }
+            if (InsideBorder(IcurrFigure - 2, JcurrFigure - 1))
+            {
+                DeterminePath(IcurrFigure - 2, JcurrFigure - 1);
+            }
+            if (InsideBorder(IcurrFigure + 2, JcurrFigure + 1))
+            {
+                DeterminePath(IcurrFigure + 2, JcurrFigure + 1);
+            }
+            if (InsideBorder(IcurrFigure + 2, JcurrFigure - 1))
+            {
+                DeterminePath(IcurrFigure + 2, JcurrFigure - 1);
+            }
+            if (InsideBorder(IcurrFigure - 1, JcurrFigure + 2))
+            {
+                DeterminePath(IcurrFigure - 1, JcurrFigure + 2);
+            }
+            if (InsideBorder(IcurrFigure + 1, JcurrFigure + 2))
+            {
+                DeterminePath(IcurrFigure + 1, JcurrFigure + 2);
+            }
+            if (InsideBorder(IcurrFigure - 1, JcurrFigure - 2))
+            {
+                DeterminePath(IcurrFigure - 1, JcurrFigure - 2);
+            }
+            if (InsideBorder(IcurrFigure + 1, JcurrFigure - 2))
+            {
+                DeterminePath(IcurrFigure + 1, JcurrFigure - 2);
+            }
+        }
+
+        public void DeactivateAllButtons()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    butts[i, j].Enabled = false;
+                }
+            }
+        }
+
+        public void ActivateAllButtons()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    butts[i, j].Enabled = true;
+                }
+            }
+        }
+
+        public void ShowDiagonal(int IcurrFigure, int JcurrFigure, bool isOneStep = false)
+        {
+            int j = JcurrFigure + 1;
+            for (int i = IcurrFigure - 1; i >= 0; i--)
+            {
+                if (InsideBorder(i, j))
+                {
+                    if (!DeterminePath(i, j))
+                        break;
+                }
+                if (j < 7)
+                    j++;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            j = JcurrFigure - 1;
+            for (int i = IcurrFigure - 1; i >= 0; i--)
+            {
+                if (InsideBorder(i, j))
+                {
+                    if (!DeterminePath(i, j))
+                        break;
+                }
+                if (j > 0)
+                    j--;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            j = JcurrFigure - 1;
+            for (int i = IcurrFigure + 1; i < 8; i++)
+            {
+                if (InsideBorder(i, j))
+                {
+                    if (!DeterminePath(i, j))
+                        break;
+                }
+                if (j > 0)
+                    j--;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            j = JcurrFigure + 1;
+            for (int i = IcurrFigure + 1; i < 8; i++)
+            {
+                if (InsideBorder(i, j))
+                {
+                    if (!DeterminePath(i, j))
+                        break;
+                }
+                if (j < 7)
+                    j++;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+        }
+
+        public void ShowVerticalHorizontal(int IcurrFigure, int JcurrFigure, bool isOneStep = false)
+        {
+            for (int i = IcurrFigure + 1; i < 8; i++)
+            {
+                if (InsideBorder(i, JcurrFigure))
+                {
+                    if (!DeterminePath(i, JcurrFigure))
+                        break;
+                }
+                if (isOneStep)
+                    break;
+            }
+            for (int i = IcurrFigure - 1; i >= 0; i--)
+            {
+                if (InsideBorder(i, JcurrFigure))
+                {
+                    if (!DeterminePath(i, JcurrFigure))
+                        break;
+                }
+                if (isOneStep)
+                    break;
+            }
+            for (int j = JcurrFigure + 1; j < 8; j++)
+            {
+                if (InsideBorder(IcurrFigure, j))
+                {
+                    if (!DeterminePath(IcurrFigure, j))
+                        break;
+                }
+                if (isOneStep)
+                    break;
+            }
+            for (int j = JcurrFigure - 1; j >= 0; j--)
+            {
+                if (InsideBorder(IcurrFigure, j))
+                {
+                    if (!DeterminePath(IcurrFigure, j))
+                        break;
+                }
+                if (isOneStep)
+                    break;
+            }
+        }
+
+        public bool DeterminePath(int IcurrFigure, int j)
+        {
+            if (map[IcurrFigure, j] == 0)
+            {
+                butts[IcurrFigure, j].BackColor = Color.Yellow;
+                butts[IcurrFigure, j].Enabled = true;
+            }
+            else
+            {
+                if (map[IcurrFigure, j] / 10 != player)
+                {
+                    butts[IcurrFigure, j].BackColor = Color.Yellow;
+                    butts[IcurrFigure, j].Enabled = true;
+                }
+                return false;
+            }
+            return true;
+        }
+
+        public bool InsideBorder(int ti, int tj)
+        {
+            if (ti >= 8 || tj >= 8 || ti < 0 || tj < 0)
+                return false;
+            return true;
+        }
+
+        public void CloseSteps()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    butts[i, j].BackColor = GetPrevButtonColor(butts[i,j]);
+                }
+            }
+        }
+
+        public void switchPlayer()
+        {
+            if (player == 1)
+            {
+                player = 2;
+                label2.Text = "CZARNE";
+            }
+            else
+            {
+                player = 1;
+                label2.Text = "BIAŁE";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            this.Controls.Add(button1);
+            this.Controls.Add(label1);
+            this.Controls.Add(label2);
+            Init();
+        }
+    }
+}
